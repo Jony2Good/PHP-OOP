7499f6ad788646647407d60985d3296f61b26565
artem_emelianenko <akk_12@mail.ru>
2023-07-28 15:16:54 +0300

    add:controller and router

---
+++ fourth/init.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once('Storage/icontroller.php');
+include_once('Storage/router.php');
+include_once('Storage/istorage.php');
+include_once('Storage/filestorage.php');
+
+include_once('Controller/controller.php');
